00e2e33c2617ba7eee00849d90fe7109a9da7a2a
noth1ng_1s_true <vorozheevkirill@gmail.com>
2025-09-26 15:36:46 +0500

    fix

--- src/main/java/studying/altasofttest/camel/VacancyNotificationRoute.java
+++ src/main/java/studying/altasofttest/camel/VacancyNotificationRoute.java
@@ -5,40 +5,6 @@ import org.springframework.stereotype.Component;
 import studying.altasofttest.dto.VacancyForSubscriber;
 import studying.altasofttest.services.implementations.VacancyNotificationService;
 
-//@Component
-//public class VacancyNotificationRoute extends RouteBuilder {
-//
-//    private final VacancyNotificationService notificationService;
-//    private final VacancyEmailProcessor emailProcessor;
-//
-//    public VacancyNotificationRoute(VacancyNotificationService notificationService,
-//                                    VacancyEmailProcessor emailProcessor) {
-//        this.notificationService = notificationService;
-//        this.emailProcessor = emailProcessor;
-//    }
-//
-//    @Override
-//    public void configure() {
-//        from("quartz://vacancyNotification?cron=0+0/2+*+*+*+?")
-//                .routeId("vacancyNotificationRoute")
-//                .bean(notificationService, "getNewVacancyNotifications")
-//                .split(body()).parallelProcessing()
-//                .process(exchange -> {
-//                    VacancyForSubscriber vfs = exchange.getIn().getBody(VacancyForSubscriber.class);
-//                    exchange.getIn().setHeader("vacancies", vfs.vacancies());
-//                    exchange.getIn().setHeader("applicant", vfs.subscriber());
-//                    exchange.getIn().setBody(vfs.vacancies());
-//                })
-//                .process(emailProcessor)
-//                .to("smtp://{{spring.mail.host}}:{{spring.mail.port}}"
-//                        + "?username={{spring.mail.username}}"
-//                        + "&password={{spring.mail.password}}"
-//                        + "&mail.smtp.auth={{spring.mail.properties.mail.smtp.auth}}"
-//                        + "&mail.smtp.starttls.enable={{spring.mail.properties.mail.smtp.starttls.enable}}");
-//    }
-//}
-
-
 @Component
 public class VacancyNotificationRoute extends RouteBuilder {
 
@@ -53,8 +19,6 @@ public class VacancyNotificationRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-
-        // Планировщик — запускается каждые 2 минуты
         from("quartz://vacancyNotification?cron=0+0/2+*+*+*+?")
                 .routeId("vacancyNotificationRoute")
                 .process(exchange ->
